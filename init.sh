0e44206e80beda620bf479ff762f15621156f23d
Jason Punyon <jason.punyon@gmail.com>
2013-01-29 01:10:52 -0500

    Initial Commit
    IT'S INITIAL!

---
+++ init.sh
@@ -0,0 +1,20 @@
+#1/bin/bash
+mkdir /opt/redis
+mkdir /opt/redis/bin
+
+cd /opt/redis
+wget http://redis.googlecode.com/files/redis-2.6.7.tar.gz
+tar xzf redis-2.6.7.tar.gz
+
+cd redis-2.6.7
+make
+
+cp src/redis-server /opt/redis/bin/redis-server
+cp src/redis-cli /opt/redis/bin/redis-cli
+
+cp /vagrant/redis.init.d /etc/init.d/redis
+cp /vagrant/redis.conf /etc/redis.conf
+
+useradd redis
+
+/etc/init.d/redis start
