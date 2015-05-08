87abf385ea24a2acc583214b09b9512a9d63edf8
mgonzo <marcos.gonzales@sheknows.com>
2015-05-08 13:19:53 -0700

    Changed to current stable redis
    Redis has moved to download.redis.io and they've
    also been kind to create a redis-stable.tar.gz so
    that we don't have to update the version.

--- init.sh
+++ init.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 mkdir /opt/redis
 mkdir /opt/redis/bin
-
 cd /opt/redis
-wget http://redis.googlecode.com/files/redis-2.6.13.tar.gz
-tar xzf redis-2.6.13.tar.gz
 
-cd redis-2.6.13
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
 make
 
 cp src/redis-server /opt/redis/bin/redis-server
